b900627e5ab5a452b4e8a9ad4b62a2b40b9ead4a
Peter Elliott <p.elliott332@gmail.com>
2017-12-27 11:27:25 -0700

    added test.sh and strlen test

---
+++ test/strlen.c
@@ -0,0 +1,11 @@
+#include <string.h>
+
+
+int main() {
+    int rval = 0;
+    rval = rval || (strlen("") == 0);
+    rval = rval || (strlen("a") == 1);
+    rval = rval || (strlen("hello world") == 11);
+    rval = rval || (strlen("aaaa\0abc") == 4);
+    rval = rval || (strlen("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa") == 92);
+}
---
+++ test/test.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+mkdir -p test/bin
+
+
+run_test() {
+    gcc -Wall -nostdlib test/$1.c bin/plibc.a -o test/bin/$1 
+    test/bin/$1
+    if [ $? == 0 ]
+    then
+        echo -e "$1: \e[32mpassed\e[00m"
+    else
+        echo -e "$1: \e[31mfailed\e[00m"
+    fi
+    return $?
+}
+
+FAILURE=0
+
+if [ -f test/$1.c ]
+then
+    run_test $1
+else
+    for f in test/*.c
+    do
+        run_test $(basename $f .c)
+        FAILURE=$(($FAILURE || $?))
+    done
+fi
+exit $FAILURE
